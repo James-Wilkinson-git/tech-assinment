ceff930c35bacb38110c54d9457a7a47267e84f2
James Wilkinson <james@jerw.ca>
2023-06-09 15:50:05 -0400

    Add broken sort button

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { prefDocuments } from "../mockData";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
+
 export const Documents = () => {
   const { selectedAccount } = useSelector((state) => state.account);
   const [documents, setDocuments] = useState();
@@ -10,20 +18,52 @@ export const Documents = () => {
     });
     setDocuments(docs[0].docs);
   }, [selectedAccount]);
+  const sortTable = () => {
+    const sortedDocs = documents.sort(function (a, b) {
+      let x = a["document_type"];
+      let y = b["document_type"];
+      return x < y ? -1 : x > y ? 1 : 0;
+    });
+    console.log(sortedDocs);
+    setDocuments(() => {
+      return sortedDocs;
+    });
+  };
 
   return (
     <>
-      <div>Documents for Account: {selectedAccount}</div>
       <div>
-        {documents &&
-          documents.map((document) => {
-            return (
-              <p>
-                {document.document_type}, {document.is_paper_flag.toString()}
-              </p>
-            );
-          })}
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => sortTable()}
+        >
+          Sort Document Type
+        </Button>
       </div>
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Document Type</TableCell>
+              <TableCell>Delivery Method</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {documents &&
+              documents.map((document) => {
+                return (
+                  <TableRow key={document.document_type}>
+                    <TableCell>{document.document_type}</TableCell>
+                    <TableCell>
+                      {document.is_paper_flag ? "Paper" : "Paperless"}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
