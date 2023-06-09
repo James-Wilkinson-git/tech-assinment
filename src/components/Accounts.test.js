54555ccaef06851a3e2cdcd4beb4393d8c9830a7
James Wilkinson <james@jerw.ca>
2023-06-09 18:47:09 -0400

    Another working test

--- src/components/Accounts.test.js
+++ src/components/Accounts.test.js
@@ -1,1 +1,17 @@
-test("Render App Bar", () => {});
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Accounts } from "./Accounts";
+import { Provider } from "react-redux";
+import store from "../redux/store";
+
+test("renders account selection correctly", () => {
+  render(
+    <Provider store={store}>
+      <Accounts />
+    </Provider>
+  );
+
+  const accountSelect = screen.getByLabelText("Account");
+
+  expect(accountSelect).toBeInTheDocument();
+});
